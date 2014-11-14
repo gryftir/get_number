7ad0c9bd925e79e544b2964c7360640907ee29e7
Lawrence Siebert <gryftir@gmail.com>
2014-11-14 00:04:55 -0800

    check for minimum argument count

--- get_number.c
+++ get_number.c
@@ -4,6 +4,10 @@
 
 int main(int argc, char *argv[])
 {
+    if(argc == 1) {
+      printf("usage: ./get_number [number]\nexample: ./get_number 24\n");
+      return 0;
+    }
     run_get_number(argv[1], "key.txt", stdout);
     return 0;
 }
